0bf0ed9eadd161fb782b1dbec985cae4f64f1525
JullianMQ <jullianq.dev@gmail.com>
2025-10-29 12:52:45 +0800

    frontend initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-center">
+                Certicode Platform
+            </h1>
+        </>
+    )
+}
+
+export default App
